51a933107edbe98d20e57478d157409acf2c2190
Potti1234 <lukas.pottner@gmail.com>
2025-05-31 17:15:58 +0200

    Edit eslint rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/no-unescaped-entities': 'off',
     },
   }),
 ];
